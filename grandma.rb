a8c405cb994a7fef8ec775ea48c262f10aab9d1f
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-13 03:37:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,11 +19,21 @@
 #end
 
 def speak_to_grandma(phrase)
-  if phrase.upcase
+  if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "Hi Nana, how are you?"
     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "Hi!"
+    "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
+    "NO, NOT SINCE 1938!"
+  else phrase == "WHAT?"
+    "NO, NOT SINCE 1938!"
+  end
+end
+
+speak_to_grandma("i love you grandma.upcase")
+    
     "HUH?! SPEAK UP, SONNY!"
   end
 end
